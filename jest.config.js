31e30a474a86962b854c375f01a096ecda941224
Pauline02102 <he201933@students.ephec.be>
2023-12-22 18:41:17 +0100

    t

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,8 @@
 module.exports = {
     preset: "jest-expo",
     transformIgnorePatterns: [
-      'node_modules/(?!(react-native|my-unsupport-module|another-module)/)',
-    ],
+      "/node_modules/(?!(@react-native|react-native)/).*/"
+  ],
     roots: ["."],
     setupFiles: ['./__mocks__/@react-native-async-storage/async-storage.js'],
   };
