b0017d24f7d2ec991c5f645726094b42775c37db
Pauline02102 <he201933@students.ephec.be>
2023-12-22 18:37:09 +0100

    test front

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets:  ["@babel/preset-env"],
+    presets: ['babel-preset-expo'],
     plugins: [
       "@babel/plugin-proposal-export-namespace-from",
       "react-native-reanimated/plugin",
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 // jest.config.js
 module.exports = {
     preset: "jest-expo",
+    transformIgnorePatterns: [
+      'node_modules/(?!(react-native|my-unsupport-module|another-module)/)',
+    ],
     roots: ["."],
     setupFiles: ['./__mocks__/@react-native-async-storage/async-storage.js'],
   };
