7668db869e569a461c6ba2020bb2be1816b2dcb8
Pauline02102 <he201933@students.ephec.be>
2023-12-29 00:49:49 +0100

    test front

--- client/jest.config.js
+++ client/jest.config.js
@@ -2,7 +2,8 @@
 module.exports = {
     preset: "jest-expo",
     transformIgnorePatterns: [
-      "/node_modules/(?!(@react-native|react-native)/).*/"
+      "/node_modules/(?!(@react-native|react-native)/).*/",
+      
   ],
     roots: ["."],
     setupFiles: ['./__mocks__/@react-native-async-storage/async-storage.js'],
