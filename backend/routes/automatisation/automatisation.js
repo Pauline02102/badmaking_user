f89d96554e68dec2cafb953b6249e2d6e5b8122c
Pauline02102 <he201933@students.ephec.be>
2023-12-28 20:09:08 +0100

    autopatisation10 h

--- backend/routes/automatisation/automatisation.js
+++ backend/routes/automatisation/automatisation.js
@@ -32,7 +32,7 @@ const cron = require('node-cron');
 // 24h : 0 0 */1 * * *
 
 if (process.env.NODE_ENV !== 'test') {
-    cron.schedule('*/30 * * * * *', async () => {
+    cron.schedule('0 */10 * * * *', async () => {
         const client = await db.connect();
         console.log("Travail Cron démarré - vérification des événements pour créer des paires");
 
