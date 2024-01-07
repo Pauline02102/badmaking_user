0a1b6904e1fbf303d4bdab64f2a216a3baa952bf
Pauline02102 <he201933@students.ephec.be>
2024-01-07 18:28:33 +0100

    chnage port

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,7 @@
 require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
-const https = require('https');
-const fs = require('fs');
+
 const paires = require("./routes/paires/paires.js");
 const automatisation = require("./routes/automatisation/automatisation.js");
 const date_color = require("./routes/date_color/date_color.js");
@@ -39,11 +38,6 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-const httpsOptions = {
-  key: fs.readFileSync('/etc/letsencrypt/live/rixbad.ovh/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/rixbad.ovh/fullchain.pem')
-};
-
 db.query('SELECT NOW()', [])
   .then(res => {
     console.log("Connexion à la base de données PostgreSQL réussie, heure actuelle :", res[0].now);
@@ -54,10 +48,11 @@ db.query('SELECT NOW()', [])
 
 
 app.use(express.json());
-// Vérifier l'environnement avant de lancer le serveur HTTPS
 if (process.env.NODE_ENV !== 'test') {
-  https.createServer(httpsOptions, app).listen(443, () => {
-    console.log('Serveur HTTPS en cours d\'exécution sur le port 443');
+  const port = process.env.PORT || 3030;
+
+  app.listen(port, () => {
+    console.log(`Serveur en cours d'exécution sur le port ${port}`);
   });
 }
 
