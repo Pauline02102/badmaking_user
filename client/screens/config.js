bda48cea87889864794fa2e41a13c5119712cc82
Pauline02102 <he201933@students.ephec.be>
2024-01-07 19:03:16 +0100

    https

--- client/screens/config.js
+++ client/screens/config.js
@@ -1,6 +1,7 @@
 // config.js
 //export const BASE_URL = 'http://192.168.1.6:3030';
-export const BASE_URL = 'http://51.77.230.223:3030';
+//export const BASE_URL = 'http://51.77.230.223:3030';
+export const BASE_URL ='https://rixbad.ovh:3030'
 
 //export const BASE_URL = 'https://badmakingdb-ff17d899eaa2.herokuapp.com';
 
